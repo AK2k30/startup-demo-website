ad1656bd9ac35ff4cf76526d5b31a02137a9b079
Akash Singh <akashsunilsingh5555@gmail.com>
2024-02-25 20:41:00 +0530

    solve

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/dist/react';
+import { SpeedInsights } from '@vercel/speed-insights/dist/next';
 import '../styles/globals.css';
 
 const RootLayout = ({ children }) => (
