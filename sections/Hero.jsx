748190d209ba60988355f8c6e5f3853c0b196bbb
Akash Singh <akashsunilsingh5555@gmail.com>
2024-02-25 21:34:38 +0530

    new

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -15,11 +15,15 @@ const Hero = () => (
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
       <div className="flex justify-center items-center flex-col relative z-10">
+        <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
+          Giveway
+        </motion.h1>
         <motion.div
           variants={textVariant(1.2)}
           className="flex flex-row justify-center items-center"
         >
-          <motion.img variants={textVariant(1.1)} className={`${styles.heroHeading} customLogoSize`} src="/giveway-logo.png" style={{ width: '900px' }} />
+          <div className={styles.heroDText} />
+          <h1 className={styles.heroHeading}>emo</h1>
         </motion.div>
       </div>
 
