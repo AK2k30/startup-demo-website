a20fe11d6a375b8fcce305a3e3a9fab6bc119ebf
Akash Singh <akashsunilsingh5555@gmail.com>
2024-02-27 22:39:09 +0530

    google

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-template-curly-in-string */
+import Script from 'next/script';
 import { ClerkProvider } from '@clerk/nextjs';
 import '../styles/globals.css';
 
@@ -7,6 +9,19 @@ const RootLayout = ({ children }) => (
       <head>
         <link rel="preconnect" href="https://stijndv.com" />
         <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-9SHMQV1FFN"
+        />
+
+        <Script id="google-analytics">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', ${'${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}'});
+          `}
+        </Script>
       </head>
       <body>
         {children}
